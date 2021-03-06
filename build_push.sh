6d038bac121097e208b84219dd06134ec230608e
Frantisek Brabec <frantisek.brabec@robotika.cz>
2021-03-06 15:37:07 -0500

    build on CUDA 11 push to robotika docker repository

--- build_push.sh
+++ build_push.sh
@@ -1,48 +1,45 @@
 #!/bin/bash
 
-for var in cpu cpu-noopt gpu gpu-cc53 gpu-cc60 gpu-cc61 gpu-cc62 gpu-cc70 gpu-cc72 gpu-cc75 gpu-cc80 gpu-cc86 \
-          cpu-cv cpu-noopt-cv gpu-cv gpu-cv-cc53 gpu-cv-cc60 gpu-cv-cc61 gpu-cv-cc62 gpu-cv-cc70 gpu-cv-cc72 gpu-cv-cc75 gpu-cv-cc80 gpu-cv-cc86
-do
-  DOCKER_REPO="daisukekobayashi/darknet"
-  SOURCE_BRANCH="master"
-  SOURCE_COMMIT=`git ls-remote https://github.com/AlexeyAB/darknet.git ${SOURCE_BRANCH} | awk '{ print $1 }'`
-  DOCKER_TAG=$var
-  VAR=$var
+ARCH="gpu-cv-cc86"
+DOCKER_REPO="robotikainspace/moon-final"
+SOURCE_BRANCH="master"
+SOURCE_COMMIT=`git ls-remote https://github.com/AlexeyAB/darknet.git ${SOURCE_BRANCH} | awk '{ print $1 }'`
+DOCKER_TAG="darknet"
 
   echo $DOCKER_REPO
   echo $SOURCE_BRANCH
   echo $SOURCE_COMMIT
   echo $DOCKER_TAG
-  echo $VAR
+  echo $ARCH
 
-if [[ "$DOCKER_TAG" == *cv* ]]; then
-  if [[ "$DOCKER_TAG" = *cpu-cv || "$DOCKER_TAG" = *cpu-noopt-cv ]]; then
+if [[ "$ARCH" == *cv* ]]; then
+  if [[ "$ARCH" = *cpu-cv || "$ARCH" = *cpu-noopt-cv ]]; then
     echo "building cpu-cv or cpu-noopt-cv"
     docker build \
-      --build-arg CONFIG=$VAR \
+      --build-arg CONFIG=$ARCH \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
       -t $DOCKER_REPO:$DOCKER_TAG -f Dockerfile.cpu-cv .
   else
     echo "building gpu-cv"
     docker build \
-      --build-arg CONFIG=$VAR \
+      --build-arg CONFIG=$ARCH \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
       -t $DOCKER_REPO:$DOCKER_TAG -f Dockerfile.gpu-cv .
   fi
  else
-  if [[ "$DOCKER_TAG" = *cpu || "$DOCKER_TAG" = *cpu-noopt ]]; then
+  if [[ "$ARCH" = *cpu || "$ARCH" = *cpu-noopt ]]; then
     echo "building cpu or cpu-noopt"
     docker build \
-      --build-arg CONFIG=$VAR \
+      --build-arg CONFIG=$ARCH \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
       -t $DOCKER_REPO:$DOCKER_TAG -f Dockerfile.cpu .
   else
     echo "building gpu"
     docker build \
-      --build-arg CONFIG=$VAR \
+      --build-arg CONFIG=$ARCH \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
       -t $DOCKER_REPO:$DOCKER_TAG -f Dockerfile.gpu .
@@ -50,5 +47,3 @@ if [[ "$DOCKER_TAG" == *cv* ]]; then
 fi
 
 docker push $DOCKER_REPO:$DOCKER_TAG
-done
-
